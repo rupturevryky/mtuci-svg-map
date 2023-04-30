a1117bee1aa6998315720bd422fd7954de0251ab
Vladislav M <vladislav.m.vlad@gmail.com>
2023-04-30 20:11:17 +0300

    start project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+// import { ReactComponent as mtuciLogo } from '../img/LogoMtuciLogo.svg';
+
+import mtuciLogos from "../img/LogoMtuciLogo.svg";
+
+
+const Header = () => {
+    return (
+        <header>
+            <img src={mtuciLogos} alt="" />
+
+
+        </header>
+    )
+
+}
+
+export default Header;
