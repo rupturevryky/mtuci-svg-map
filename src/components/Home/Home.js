c11eb6a26c37768bcbb836cc1a8f20675ae47771
Vladislav M <vladislav.m.vlad@gmail.com>
2023-05-04 22:33:16 +0300

    BrowserRouter

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+// import svgMtuci from '../svg/FullMtuci.svg';
+import { svgMtuci } from "../SvgMtuci/svgMtuci"
+
+export const Home = () => {
+    return (
+        <>
+            {/* <img src={svgMtuci} alt="svgNtuci" /> */}
+            <svgMtuci />
+            <div>Home page</div>
+        </>
+    );
+};
+
