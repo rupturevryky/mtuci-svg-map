0775163901b1ff2423315cbc7c36c32d128573e0
Vladislav M <vladislav.m.vlad@gmail.com>
2023-05-09 15:35:00 +0300

    end home

--- src/components/EnclosuresListItem/EnclosuresListItem.js
+++ src/components/EnclosuresListItem/EnclosuresListItem.js
@@ -4,37 +4,42 @@ const EnclosuresListItem = ({ title, active, blure, color, id, onClick, onMouseE
 
     const item = () => {
 
-        const padding = (id) => id === 1 ? `${s.string} ${s.padding}` : `${s.string}`
-        // const light = () => `${s.string} ${s.padding}`
+        const padding = () => id === 1 ? `${s.string} ${s.padding}` : `${s.string}`
 
         if (active === false) {
             if (blure === true) {
                 return (
-                    <div className={padding(id)}>
+                    <div className={padding()}>
                         <div className={s.circle} style={{ backgroundColor: color }}></div><p>{title}</p>
                     </div>
                 )
             } else {
                 return (
-                    <div className={padding(id)}>
+                    <div className={padding()}>
                         <div className={s.circle}></div><p>{title}</p>
                     </div>
                 )
             }
         } else {
             return (
-                <div className={padding(id)}>
+                <div className={padding()}>
                     <div className={s.circle} style={{ backgroundColor: color }}></div><p>{title}</p>
                 </div>
+
             )
         }
     }
+
+    const clip = () => active === true ? `${s.сlip}` : `${s.displayNone}`
+    const marginClip = () => id === 1 ? `${s.margin} ${clip()}` : `${clip()}`
+
     return (
         <li className={`${s.highlight} ${s.item}`}
             onClick={onClick}
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}>
             {item()}
+            <div className={marginClip()} style={{ backgroundColor: color }}></div>
         </li>
     );
 }
